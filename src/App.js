e947d717518484e9f590c64e6faf227c55417c99
Amy Schillerstrom <amyschillerstrom@amys-mbp.attlocal.net>
2018-06-13 11:09:00 -0500

    created basic friend card to test, more structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from "react";
-import Counter from "./components/Counter";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
+import friends from "./friends.json";
 
-const App = () => <Counter />;
+const App = () => (
+  <Wrapper>
+    <Title>Friends List</Title>
+    <FriendCard
+      
+      image={friends[0].image}
+      
+    />
+    <FriendCard
+      
+      image={friends[1].image}
+      
+    />
+    <FriendCard
+      
+      image={friends[2].image}
+      
+    />
+  </Wrapper>
+);
 
 export default App;
+
