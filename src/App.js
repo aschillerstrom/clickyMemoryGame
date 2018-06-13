fe1febd8d76b38a9149596cda056959eb2e558ee
Amy Schillerstrom <amyschillerstrom@amys-mbp.attlocal.net>
2018-06-13 12:56:25 -0500

    added title, nav and a wrapper

--- src/App.js
+++ src/App.js
@@ -1,29 +1,106 @@
-import React from "react";
+import React, { Component } from "react";
+import FriendCard from "./components/friendCard";
+import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import FriendCard from "./components/FriendCard";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
 import friends from "./friends.json";
+import "./App.css";
 
-const App = () => (
-  <Wrapper>
-    <Title>Friends List</Title>
-    <FriendCard
-      
-      image={friends[0].image}
-      
-    />
-    <FriendCard
-      
-      image={friends[1].image}
-      
-    />
-    <FriendCard
-      
-      image={friends[2].image}
-      
-    />
-  </Wrapper>
-);
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+class App extends Component {
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Go!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          title="Oodles of Poodles"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Title>
+          Try to click on each poodle just once - but only click on them once or
+          you'll be sad as hound dog's eye!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
-
--- src/components/friendCard/friendCard.js
+++ src/components/friendCard/friendCard.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./FriendCard.css";
+import "./friendCard.css";
 
-const FriendCard = props => (
+const friendCard = props => (
   <div className="card">
     <div className="img-container">
       <img alt={props.name} src={props.image} />
@@ -9,4 +9,4 @@ const FriendCard = props => (
   </div>
 );
 
-export default FriendCard;
+export default friendCard;
