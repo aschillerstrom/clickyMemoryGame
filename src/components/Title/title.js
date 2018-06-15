d21c68048b938bd2cf67c6a81f5a6bfa774b6c94
Amy Schillerstrom <amyschillerstrom@amys-mbp.attlocal.net>
2018-06-15 18:59:17 -0500

    fixed header

--- src/components/Title/title.js
+++ src/components/Title/title.js
@@ -3,8 +3,9 @@ import "./title.css";
 
 const Title = props => (
 <div className = "header">
-    <h1 className="title">{props.children}</h1>
-    <div className="scores">Score: {props.score} Highscore: {props.highscore}</div>
+    <h1>Oodles of Poodles</h1>
+    <h3>Test your memory - click on each poodle only once!</h3>
+    <h3 className="scores">Score: {props.score} Highscore: {props.highscore}</h3>
 </div>
 );
 
